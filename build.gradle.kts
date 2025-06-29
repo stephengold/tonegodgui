37985760c6e4f553ce3b7024789ef1bb6288d030
stephengold <sgold@sonic.net>
2025-06-29 15:44:43 -0700

    test JME v3.9.0-alpha2

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ java {
 }
 
 dependencies {
-    api("org.jmonkeyengine:jme3-core:3.8.1-stable")
+    api("org.jmonkeyengine:jme3-core:3.9.0-alpha2")
 }
 
 tasks.withType<JavaCompile>().all { // Java compile-time options:
